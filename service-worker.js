0baafb9f62498312674cbb70885cce72f279d177
Muchabzz <kamil26082005@wp.pl>
2026-04-28 14:48:17 +0200

    poprawki funkcji

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "grafik-pwa-v7";
+const CACHE_NAME = "grafik-pwa-v9";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -24,6 +24,18 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then(cached =>
       cached || fetch(event.request).catch(() => caches.match("./index.html"))
